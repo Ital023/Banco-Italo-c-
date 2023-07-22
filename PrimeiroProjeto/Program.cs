2047952b77fdec1d062074aeee00dd5931815eeb
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-07-22 17:50:53 -0300

    att

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,8 +1,8 @@
-﻿exibirLogo();
+﻿List<string> usuarios = new List<string>();
 
+exibirMenu();
 
-
-void exibirLogo()
+void exibirHeader()
 {
     Console.WriteLine(@"
 ██████╗░░█████╗░███╗░░██╗░█████╗░░█████╗░  ██╗████████╗░█████╗░██╗░░░░░░█████╗░
@@ -20,3 +20,98 @@ void msgBoasVindas()
     Console.WriteLine("Boas vindas ao banco Italo");
 }
 
+void exibirMenu()
+{
+    int menuTemp = 0;
+
+    while (menuTemp != -1)
+    {
+        exibirHeader();
+
+        Console.WriteLine("\nDigite 1 para cadastrar usuario");
+        Console.WriteLine("Digite 2 para remover usuario");
+        Console.WriteLine("Digite 3 para alterar usuario");
+        Console.WriteLine("Digite 4 para exibir usuarios");
+        Console.WriteLine("Digite 5 para limpar console");
+        Console.WriteLine("Digite -1 para sair");
+        Console.Write("\nInsira o numero: ");
+        string opcao = Console.ReadLine();
+        int opcaoNumerica = int.Parse(opcao);
+
+        switch (opcaoNumerica)
+        {
+            case 1:
+                string nome = inputUser();
+                registrarUsuario(nome);
+                break;
+
+            case 2:
+                string nomeR = inputUser();
+                RemoverUsuario(nomeR);
+                break;
+            case 4:
+                int i = 0;
+                foreach(string usuario in usuarios)
+                {
+                    Console.WriteLine("\nID: "+ i + " Nome: " + usuario);
+                    i++;
+                }
+                break;
+            case 5:
+                limparConsole();
+                break;
+
+            case -1:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+
+}
+
+string inputUser()
+{
+    Console.Write("Insira o usuario: ");
+    string nomeRemover = Console.ReadLine();
+
+    return nomeRemover;
+} 
+
+
+void registrarUsuario(string usuario)
+{
+    usuarios.Add(usuario);
+}
+
+void RemoverUsuario(string nomeRemover)
+{
+    foreach (string usuario in usuarios)
+    {
+        if (nomeRemover.Equals(usuario))
+        {
+            usuarios.Remove(usuario);
+        }
+    }
+}
+
+void alterarUsuario(string usuario)
+{
+    string novoUser = inputUser();
+    int i = 0;
+
+    foreach(string user in usuarios)
+    {
+        if (usuarios.Equals(usuario))
+        {
+            usuarios[i] = novoUser;
+        }
+    }
+    i++;
+}
+
+
+void limparConsole()
+{
+    Console.Clear();
+}
